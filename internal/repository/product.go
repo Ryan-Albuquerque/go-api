1e4b2d8714693c02467916c37e59458529470b0a
ryan-albuquerque <ryan.albuquerque2002@gmail.com>
2024-08-27 23:10:19 -0400

    feat: Add product repository and GetProducts method

---
+++ internal/repository/product.go
@@ -0,0 +1,37 @@
+package repository
+
+import (
+	"database/sql"
+
+	"github.com/Ryan-Albuquerque/go-api/internal/model"
+)
+
+type ProductRepository struct {
+	db *sql.DB
+}
+
+func NewProductRepository(db *sql.DB) *ProductRepository {
+	return &ProductRepository{
+		db: db,
+	}
+}
+
+func (pr *ProductRepository) GetProducts() ([]model.Product, error) {
+	rows, err := pr.db.Query("SELECT * FROM products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []model.Product
+	for rows.Next() {
+		var product model.Product
+		err := rows.Scan(&product.ID, &product.Name, &product.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, product)
+	}
+
+	return products, nil
+}
